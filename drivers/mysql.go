d141b46db1617f1a7618cebf1d1e57ea1a2a2c86
jinchunguang <jinchunguang@jiayunhui.com>
2019-08-09 18:27:42 +0800

    add files

--- drivers/mysql.go
+++ drivers/mysql.go
@@ -3,7 +3,12 @@ package drivers
 
 import (
 	"database/sql"
+	"fmt"
+	"strconv"
+	"time"
+	"bingo_service/pkgs/config"
 	_ "github.com/go-sql-driver/mysql"
+
 )
 
 // query need rows.Close to release db ins
@@ -13,38 +18,38 @@ var MysqlDbErr error // db err instance
 
 func init() {
 
-	//// 获取db配置
-	//dbConfig := config.GetDbConfig()
-	//
-	//dbDSN := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s",
-	//	dbConfig["db_user"],
-	//	dbConfig["db_pwd"],
-	//	dbConfig["db_host"],
-	//	dbConfig["db_port"],
-	//	dbConfig["db_name"],
-	//	dbConfig["db_charset"],
-	//)
-	//
-	//// 打开db
-	//MysqlDb, MysqlDbErr = sql.Open("mysql", dbDSN)
-	//
-	//if MysqlDbErr != nil {
-	//	panic("database data source name error: " + MysqlDbErr.Error())
-	//}
-	//
-	//// 连接池最大连接数
-	//dbMaxOpenConns, _ := strconv.Atoi(dbConfig["db_max_open_conns"])
-	//MysqlDb.SetMaxOpenConns(dbMaxOpenConns)
-	//
-	//// 连接池最大空闲数
-	//dbMaxIdleConns, _ := strconv.Atoi(dbConfig["db_max_idle_conns"])
-	//MysqlDb.SetMaxIdleConns(dbMaxIdleConns)
-	//
-	//// 连接池链接最长生命周期
-	//dbMaxLifetimeConns, _ := strconv.Atoi(dbConfig["db_max_lifetime_conns"])
-	//MysqlDb.SetConnMaxLifetime(time.Duration(dbMaxLifetimeConns))
-	//
-	//if MysqlDbErr = MysqlDb.Ping(); nil != MysqlDbErr {
-	//	panic("database connect failed: " + MysqlDbErr.Error())
-	//}
+	// 获取db配置
+	dbConfig := config.GetDbConfig()
+
+	dbDSN := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s",
+		dbConfig["db_user"],
+		dbConfig["db_pwd"],
+		dbConfig["db_host"],
+		dbConfig["db_port"],
+		dbConfig["db_name"],
+		dbConfig["db_charset"],
+	)
+
+	// 打开db
+	MysqlDb, MysqlDbErr = sql.Open("mysql", dbDSN)
+
+	if MysqlDbErr != nil {
+		panic("database data source name error: " + MysqlDbErr.Error())
+	}
+
+	// 连接池最大连接数
+	dbMaxOpenConns, _ := strconv.Atoi(dbConfig["db_max_open_conns"])
+	MysqlDb.SetMaxOpenConns(dbMaxOpenConns)
+
+	// 连接池最大空闲数
+	dbMaxIdleConns, _ := strconv.Atoi(dbConfig["db_max_idle_conns"])
+	MysqlDb.SetMaxIdleConns(dbMaxIdleConns)
+
+	// 连接池链接最长生命周期
+	dbMaxLifetimeConns, _ := strconv.Atoi(dbConfig["db_max_lifetime_conns"])
+	MysqlDb.SetConnMaxLifetime(time.Duration(dbMaxLifetimeConns))
+
+	if MysqlDbErr = MysqlDb.Ping(); nil != MysqlDbErr {
+		panic("database connect failed: " + MysqlDbErr.Error())
+	}
 }
